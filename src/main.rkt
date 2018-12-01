983e27d7dbc73036fc4f95cb60e2efa4e66fa948
Ricardo Vela <aricav96@gmail.com>
2018-11-30 19:38:24 -0600

    Consume function and name to function dispatch
    - consume dummy header of wack protocol
    - apply dummy functions to commands
    - name->function function (haha)
    - new consume function, refactored let from handle.
    -

--- src/main.rkt
+++ src/main.rkt
@@ -14,38 +14,85 @@
 ;; Calls the message handler
 (define (accept-handle listener)
   (define-values (in out) (tcp-accept listener))
+  (consume in)
+  (write "ACK" out)
+  (flush-output out)
+  (consume in)
+  (write "ACK" out)
+  (flush-output out)
+  (consume in)
+  (write "ACK" out)
+  (flush-output out)
+  (consume in)
+  (write "ACK" out)
+  (flush-output out)
+  (consume in)
+  (write "ACK" out)
+  (flush-output out)
   (handle in out)
+  (displayln "Closing Connection...")
   (close-output-port out)
   (close-input-port in))
+
+;; Consumes all the information available from port in, and returns it as a
+;; string
+(define (consume in)
+  (let* ([buffer (make-bytes 256)]
+         [length (read-bytes-avail! buffer in)])
+    (if (eof-object? length)
+        empty
+        (bytes->string/utf-8 (subbytes buffer 0 length)))))
 
 ;; Message handler.
 ;; Receives input and output ports and reads them into a buffer.
 ;; Processes the client's commands and sends answer from server to client
 (define (handle in out)
-  (let* ([buffer (make-bytes 256)]
-         [length (read-bytes-avail! buffer in)])
-    (when (not (eof-object? length))
-      (define msg (bytes->string/utf-8 (subbytes buffer 0 length)))
-      (printf "Server recibió: ~a~n" msg)
-      (displayln (process msg) out)
-      (flush-output out)
-      (flush-output)
-      (handle in out))))
+  (define msg (consume in))
+  (when (not (empty? msg))
+    (printf "Server recibió: ~a~n" msg)
+    (displayln (process msg) out)
+    (flush-output out)
+    (flush-output)
+    (handle in out)))
+
+(define (name->function name)
+  (case name
+    [("CreateP") createP]
+    [("Address") address]
+    [("Fin")  fin]
+    [("End") end]
+    [else no-command]))
 
 (define (process msg)
-  msg)
+  (define string-list (tokenizer msg))
+  (define f (name->function (car string-list)))
+  (apply f (cdr string-list)))
+
+(define (createP s n)
+  (format "s:~a n:~a" s n))
+
+(define (address pid v)
+  (format "pid:~a v:~a" pid v))
+
+(define (fin pid)
+  (format "pid:~a" pid))
+
+(define (end)
+  (format "The End."))
+(define (no-command . sink)
+  (displayln "This Command name does not exist"))
 
 ;; Changes the message sent by the client into a list of strings
 (define (tokenizer msg)
   (define string-list (string-split msg))
-  (display string-list))
+  (strip-comments string-list))
 
 ;; Function to remove comments from the tokenized message
-(define (strip-comments string-listn)
-  (take-until (lambda (x) (string-contains? x "/"))))
+(define (strip-comments string-list)
+  (take-until (lambda (x) (string-contains? x "/")) string-list))
 
 ;; Function to take elements from a list until predicate is true
 (define (take-until predicate list)
-  (if (predicate (car list))
-      '()
-      (cons (car list) (take-until predicate (cdr list)))))
+  (cond [(empty? list) empty]
+        [(predicate (car list)) empty]
+        [else (cons (car list) (take-until predicate (cdr list)))]))
